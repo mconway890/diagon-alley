efc97023d24b9e0e3c9f341356b40bd63d275134
Michele Conway <mconway890@gmail.com>
2019-06-06 09:38:58 -0400

    Remove unnecessary links, restrict actions to index and show in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :stores
+  resources :stores, only: [:show, :index]
+
+  root 'stores#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
