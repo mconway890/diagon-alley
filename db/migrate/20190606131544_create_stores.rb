dc8f39eab46a0dd3fa8fd7e04b2e66fcbe095212
Michele Conway <mconway890@gmail.com>
2019-06-06 09:16:36 -0400

    Generate a store scaffold with model attributes

---
+++ db/migrate/20190606131544_create_stores.rb
@@ -0,0 +1,11 @@
+class CreateStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :stores do |t|
+      t.string :name
+      t.integer :founded
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
