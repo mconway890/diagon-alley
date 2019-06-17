4a8be0f5288f44a739a9b4e371aa9cde5ded80a6
Michele Conway <mconway890@gmail.com>
2019-06-17 15:11:21 -0400

    Text tagline attribute added to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_131544) do
+ActiveRecord::Schema.define(version: 2019_06_17_190755) do
 
   create_table "stores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_06_06_131544) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "tagline"
   end
 
 end
