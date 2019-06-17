84abd80691be55629aa3a7c41dd9f8782bf498a2
Michele Conway <mconway890@gmail.com>
2019-06-17 15:06:22 -0400

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-stores = Store.create([{name: 'Ollivanders', description: 'Wand Shop', location: 'Diagon Alley'},
-                       {name: 'Potages Cauldron Shop', description: 'Cauldron Shop', location: 'Knockturn Alley'},
-                       {name: 'Flourish and Blotts', description: 'Book Shop', location: 'Diagon Alley'},
-                       {name: 'Dystyl Phaelanges', description: 'Bones & Fossils Shop', location: 'Knockturn Alley'}])
+stores = Store.create([{name: 'Ollivanders', description: 'Ollivanders is a wand shop founded in 382 B.C. Located in Diagon Alley South Side in London, England, it is owned by the Ollivander family, widely acknowledged to be the best wandmakers in Great Britain.', tagline: 'Makers of Fine Wands since 382 BC', location: 'Diagon Alley'},
+                       {name: 'Potages Cauldron Shop', description: 'Potages Cauldron Shop is a shop on the North Side, Diagon Alley that sells all types of cauldrons', tagline: 'Cauldrons — All Sizes — Copper, Brass, Pewter, Silver — Self-Stirring — Collapsible', location: 'Knockturn Alley'},
+                       {name: 'Flourish and Blotts', description: 'Flourish and Blotts Bookseller is a bookshop in North Side, Diagon Alley, about halfway down the street on the left hand side. Established in 1654, it is where most Hogwarts students purchase their schoolbooks.', tagline: 'Bookseller since 1654', location: 'Diagon Alley'},
+                       {name: 'Dystyl Phaelanges', description: 'Dystyl Phaelanges is a shop in Knockturn Alley, specialising in bones, fossils and fangs, as well as pieces of furniture made from bone.', tagline: 'Here to lend a hand', location: 'Knockturn Alley'}])
