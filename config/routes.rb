6e917f47ffd79eea5c87104f5d8c6d8a49d589b5
Michele Conway <mconway890@gmail.com>
2019-06-18 14:29:10 -0400

    Add dining route for stores

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :stores, only: [:show, :index]
 
+  get '/dining' => 'stores#dining'
+
   root 'stores#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
