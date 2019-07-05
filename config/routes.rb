706bb01542434d3bc560a0af27ca41d2b25d2db5
Michele Conway <mconway890@gmail.com>
2019-07-05 16:19:30 -0400

    Add background video to landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
+  
   resources :stores, only: [:show, :index]
   resources :restaurants, only: [:show, :index]
 
   get '/map' => 'stores#map'
   get '/hours' => 'stores#hours'
 
-  root 'stores#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
